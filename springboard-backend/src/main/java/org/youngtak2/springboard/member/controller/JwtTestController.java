348daa6b1e11264656331bef89f7645d9ac0098d
0tak2 <0tak2.code@gmail.com>
2023-03-07 09:13:47 +0900

    트랜잭션 매니저 적용

---
+++ springboard-backend/src/main/java/org/youngtak2/springboard/member/controller/JwtTestController.java
@@ -0,0 +1,8 @@
+package org.youngtak2.springboard.member.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class JwtTestController {
+
+}
--- springboard-backend/src/main/java/org/youngtak2/springboard/member/service/MemberServiceImpl.java
+++ springboard-backend/src/main/java/org/youngtak2/springboard/member/service/MemberServiceImpl.java
@@ -2,18 +2,28 @@ package org.youngtak2.springboard.member.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.youngtak2.springboard.member.dao.MemberDao;
 import org.youngtak2.springboard.member.vo.Member;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
+	private final PlatformTransactionManager transactionManager;
+	public MemberServiceImpl(PlatformTransactionManager transactionManager) {
+	    this.transactionManager = transactionManager;
+	  }
+	
 	@Autowired
 	private MemberDao dao;
 	
 	@Override
 	public boolean registerNewAccount(Member newMember) {
 		int affectedRows = 0;
+		TransactionStatus txStatus =
+		        transactionManager.getTransaction(new DefaultTransactionDefinition());
 		
 		try {
 			affectedRows = dao.insert(newMember);
@@ -22,8 +32,10 @@ public class MemberServiceImpl implements MemberService {
 		}
 		
 		if (affectedRows == 1) {
+			transactionManager.commit(txStatus);
 			return true;
 		} else {
+			transactionManager.rollback(txStatus);
 			return false;
 		}
 	}
